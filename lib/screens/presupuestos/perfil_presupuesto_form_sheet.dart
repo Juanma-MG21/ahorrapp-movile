06ad3599dec3f075fc25de813a15adc537bb40e9
Juanma-MG21 <juanma1608g@gmail.com>
2026-09-07 21:59:16 -0500

    arreglo de bug por impor fantasma

--- lib/screens/presupuestos/perfil_presupuesto_form_sheet.dart
+++ lib/screens/presupuestos/perfil_presupuesto_form_sheet.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../../core/theme/app_theme.dart';
+// import'../../core/theme/app_theme.dart';
 import '../../core/theme/design_tokens.dart';
 import '../../models/presupuesto_model.dart';
 import '../../providers/presupuesto_provider.dart';
